303dca834b12405a4eb1fef6f86bc7d5194fc43c
viktoryboykova <viktoryboykova@list.ru>
2021-12-09 13:18:33 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -15,16 +15,18 @@ public class SqlRuParse {
     }
 
     public void parseHtml() throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements forumTable = doc.select("table.forumTable");
-        Elements rows = forumTable.select("tr");
-        for (Element row : rows) {
-            Element date = row.select("td.altCol").last();
-            if (date == null) {
-                continue;
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect(String.format("https://www.sql.ru/forum/job-offers/%s", i)).get();
+            Elements forumTable = doc.select("table.forumTable");
+            Elements rows = forumTable.select("tr");
+            for (Element row : rows) {
+                Element date = row.select("td.altCol").last();
+                if (date == null) {
+                    continue;
+                }
+                SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+                System.out.println(sqlRuDateTimeParser.parse(date.text()));
             }
-            SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
-            System.out.println(sqlRuDateTimeParser.parse(date.text()));
         }
     }
 }
